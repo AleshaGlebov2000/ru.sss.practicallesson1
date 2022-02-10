a292fe2cb4b5e3190c40f5658752157f3fb3dbcc
AleshaGlebov2000 <alesha_glebov@bk.ru>
2022-02-10 22:52:29 +0300

    Заливаем домашку

---
+++ src/main/java/ru/sss/practicallesson/Practical.java
@@ -0,0 +1,15 @@
+package ru.sss.practicallesson;
+
+public class Practical {
+    public static void main(String[] args) {
+        byte a = 30;
+        short b = 11111;
+        int c = 100000;
+        long d = 300000L;
+        float e = 12.25f;
+        double f = 123.125;
+        char g =  '\u0000';
+        boolean h = false;
+
+    }
+}
